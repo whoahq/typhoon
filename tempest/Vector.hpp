0ebe6450a03885c2e433419b363f99fd9e95df16
fallenoak <git@fallenoak.me>
2020-11-26 12:40:31 -0600

    feat(vector): add C2Vector

--- tempest/Vector.hpp
+++ tempest/Vector.hpp
@@ -1,6 +1,7 @@
 #ifndef TEMPEST_VECTOR_HPP
 #define TEMPEST_VECTOR_HPP
 
+#include "tempest/vector/C2Vector.hpp"
 #include "tempest/vector/C3Vector.hpp"
 #include "tempest/vector/C4Vector.hpp"
 
---
+++ tempest/vector/C2Vector.hpp
@@ -0,0 +1,11 @@
+#ifndef TEMPEST_VECTOR_C_2VECTOR_HPP
+#define TEMPEST_VECTOR_C_2VECTOR_HPP
+
+class C2Vector {
+    public:
+    // Member variables
+    float x = 0.0f;
+    float y = 0.0f;
+};
+
+#endif
