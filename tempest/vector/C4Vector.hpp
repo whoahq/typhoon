3c001d3ed8bca066c4bf87f469b3c26aa529f9d4
Joshua Achorn <superp00t@tutanota.com>
2021-03-06 01:47:36 -0500

    feat(vector): add list initialization constructor for C4Vector

--- tempest/vector/C4Vector.hpp
+++ tempest/vector/C4Vector.hpp
@@ -8,6 +8,13 @@ class C4Vector {
     float y = 0.0f;
     float z = 0.0f;
     float w = 0.0f;
+
+    // Member functions
+    C4Vector(float x, float y, float z, float w)
+        : x(x)
+        , y(y)
+        , z(z)
+        , w(w) {};
 };
 
 #endif
