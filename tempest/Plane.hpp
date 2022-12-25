28b22e3bbdc0085a2dfc0e09a9b77c5001552d7f
fallenoak <git@fallenoak.me>
2022-12-24 23:02:01 -0600

    feat(plane): add C4Plane

---
+++ tempest/Plane.hpp
@@ -0,0 +1,6 @@
+#ifndef TEMPEST_PLANE_HPP
+#define TEMPEST_PLANE_HPP
+
+#include "tempest/plane/C4Plane.hpp"
+
+#endif
---
+++ tempest/plane/C4Plane.hpp
@@ -0,0 +1,13 @@
+#ifndef TEMPEST_PLANE_C_4PLANE_HPP
+#define TEMPEST_PLANE_C_4PLANE_HPP
+
+#include "tempest/Vector.hpp"
+
+class C4Plane {
+    public:
+    // Member variables
+    C3Vector n;
+    float d;
+};
+
+#endif
