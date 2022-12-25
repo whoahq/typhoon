83934acd7186129e28bcbd87fb3b69b5f5eacec6
fallenoak <git@fallenoak.me>
2022-12-24 23:10:21 -0600

    feat(sphere): add CAaSphere

---
+++ tempest/Sphere.hpp
@@ -0,0 +1,6 @@
+#ifndef TEMPEST_SPHERE_HPP
+#define TEMPEST_SPHERE_HPP
+
+#include "tempest/sphere/CAaSphere.hpp"
+
+#endif
---
+++ tempest/sphere/CAaSphere.hpp
@@ -0,0 +1,13 @@
+#ifndef TEMPEST_SPHERE_C_AASPHERE_HPP
+#define TEMPEST_SPHERE_C_AASPHERE_HPP
+
+#include "tempest/Vector.hpp"
+
+class CAaSphere {
+    public:
+    // Member variables
+    C3Vector c;
+    float r;
+};
+
+#endif
