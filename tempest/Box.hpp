697c011eb9efef05effbd4c19bd95b2eb3a6330d
fallenoak <git@fallenoak.me>
2022-12-24 23:08:12 -0600

    feat(box): add CAaBox

--- tempest/Box.hpp
+++ tempest/Box.hpp
@@ -1,6 +1,7 @@
 #ifndef TEMPEST_BOX_HPP
 #define TEMPEST_BOX_HPP
 
+#include "tempest/box/CAaBox.hpp"
 #include "tempest/box/CBoundingBox.hpp"
 
 #endif
---
+++ tempest/box/CAaBox.hpp
@@ -0,0 +1,13 @@
+#ifndef TEMPEST_BOX_C_AABOX_HPP
+#define TEMPEST_BOX_C_AABOX_HPP
+
+#include "tempest/Vector.hpp"
+
+class CAaBox {
+    public:
+    // Member variables
+    C3Vector b;
+    C3Vector t;
+};
+
+#endif
