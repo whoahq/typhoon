84735d9a27f890825967f8a02043643eba80f54e
fallenoak <git@fallenoak.me>
2025-10-12 16:56:08 -0500

    feat(matrix): add C33Matrix

--- tempest/Matrix.hpp
+++ tempest/Matrix.hpp
@@ -1,6 +1,7 @@
 #ifndef TEMPEST_MATRIX_HPP
 #define TEMPEST_MATRIX_HPP
 
+#include "tempest/matrix/C33Matrix.hpp"
 #include "tempest/matrix/C44Matrix.hpp"
 
 #endif
---
+++ tempest/matrix/C33Matrix.hpp
@@ -0,0 +1,31 @@
+#ifndef TEMPEST_MATRIX_C_33MATRIX_HPP
+#define TEMPEST_MATRIX_C_33MATRIX_HPP
+
+class C33Matrix {
+    public:
+    // Member variables
+    float a0 = 1.0f;
+    float a1 = 0.0f;
+    float a2 = 0.0f;
+    float b0 = 0.0f;
+    float b1 = 1.0f;
+    float b2 = 0.0f;
+    float c0 = 0.0f;
+    float c1 = 0.0f;
+    float c2 = 1.0f;
+
+    // Member functions
+    C33Matrix() = default;
+    C33Matrix(float a0, float a1, float a2, float b0, float b1, float b2, float c0, float c1, float c2)
+    : a0(a0)
+    , a1(a1)
+    , a2(a2)
+    , b0(b0)
+    , b1(b1)
+    , b2(b2)
+    , c0(c0)
+    , c1(c1)
+    , c2(c2) {}
+};
+
+#endif
