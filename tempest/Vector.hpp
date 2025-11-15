e0eb4f12fb4e0d5bdd1d6a348ee2466f67b8197b
fallenoak <git@fallenoak.me>
2025-11-15 15:55:06 -0600

    feat(vector): add C3iVector

--- tempest/Vector.hpp
+++ tempest/Vector.hpp
@@ -4,6 +4,7 @@
 #include "tempest/vector/C2Vector.hpp"
 #include "tempest/vector/C2iVector.hpp"
 #include "tempest/vector/C3Vector.hpp"
+#include "tempest/vector/C3iVector.hpp"
 #include "tempest/vector/C4Vector.hpp"
 #include "tempest/vector/CImVector.hpp"
 
---
+++ tempest/vector/C3iVector.hpp
@@ -0,0 +1,14 @@
+#ifndef TEMPEST_VECTOR_C_3IVECTOR_HPP
+#define TEMPEST_VECTOR_C_3IVECTOR_HPP
+
+#include <cstdint>
+
+class C3iVector {
+    public:
+    // Member variables
+    int32_t x = 0;
+    int32_t y = 0;
+    int32_t z = 0;
+};
+
+#endif
