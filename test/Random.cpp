1c9e7831c874068e7c939a7dea8790eef6513d78
fallenoak <git@fallenoak.me>
2026-01-04 19:33:20 -0600

    chore(test): improve CRandom::dice test

--- test/Random.cpp
+++ test/Random.cpp
@@ -5,29 +5,22 @@ TEST_CASE("CRandom::dice", "[random]") {
     SECTION("generates numbers within range") {
         auto seed = CRndSeed(0xC0D1E2F3);
 
-        auto result1 = CRandom::dice(0, seed);
-        REQUIRE(result1 == 0);
+        for (int32_t i = 0; i < 200; i++) {
+            auto result1 = CRandom::dice(0, seed);
+            REQUIRE(result1 == 0);
 
-        auto result2 = CRandom::dice(1, seed);
-        REQUIRE(result2 == 0);
+            auto result2 = CRandom::dice(1, seed);
+            REQUIRE(result2 == 0);
 
-        auto result3 = CRandom::dice(1, seed);
-        REQUIRE(result3 == 0);
+            auto result3 = CRandom::dice(2, seed);
+            REQUIRE(result3 < 2);
 
-        auto result4 = CRandom::dice(2, seed);
-        REQUIRE(result4 < 2);
+            auto result4 = CRandom::dice(10, seed);
+            REQUIRE(result4 < 10);
 
-        auto result5 = CRandom::dice(2, seed);
-        REQUIRE(result5 < 2);
-
-        auto result6 = CRandom::dice(10, seed);
-        REQUIRE(result6 < 10);
-
-        auto result7 = CRandom::dice(10, seed);
-        REQUIRE(result7 < 10);
-
-        auto result8 = CRandom::dice(10, seed);
-        REQUIRE(result8 < 10);
+            auto result5 = CRandom::dice(100, seed);
+            REQUIRE(result5 < 100);
+        }
     }
 }
 
